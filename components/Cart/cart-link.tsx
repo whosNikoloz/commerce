e39b6b404277c88f9e6c1e7191ad0a814491f92d
whosNikoloz <nika.kobaidze021@gmail.com>
2025-02-18 23:17:26 +0400

    sss

--- components/Cart/cart-link.tsx
+++ components/Cart/cart-link.tsx
@@ -1,40 +1,45 @@
 import React, { useEffect, useRef } from "react";
-import { useCart } from "@/app/context/cartContext";
 import { Button } from "@heroui/button";
-import {  ShoppingCartIcon } from "../icons";
 import { Badge } from "@heroui/badge";
 import Link from "next/link";
+
+import { ShoppingCartIcon } from "../icons";
+
+import { useCart } from "@/app/context/cartContext";
 
 export default function Cartlink() {
   const { cart } = useCart();
   const quantityRef = useRef(cart?.length);
 
   useEffect(() => {
-    if (cart?.length && cart.length !== quantityRef.current && cart.length > 0) {
+    if (
+      cart?.length &&
+      cart.length !== quantityRef.current &&
+      cart.length > 0
+    ) {
       quantityRef.current = cart.length;
     }
   }, [cart?.length]);
 
-
   const totalQuantity = cart.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
-    <Badge 
-        color="danger"
-        size="sm"
-        variant="shadow"
-        content={totalQuantity}
-        className="border-0 absolute top-2 right-2"
-            >
-        <Button
-        variant="solid"
+    <Badge
+      className="border-0 absolute top-2 right-2"
+      color="danger"
+      content={totalQuantity}
+      size="sm"
+      variant="shadow"
+    >
+      <Button
         isIconOnly
-        className="relative rounded-full bg-transparent"
         as={Link}
+        className="relative rounded-full bg-transparent"
         href="/cart"
-            >
-        <ShoppingCartIcon  size={30}/>
-        </Button>
+        variant="solid"
+      >
+        <ShoppingCartIcon size={30} />
+      </Button>
     </Badge>
   );
 }

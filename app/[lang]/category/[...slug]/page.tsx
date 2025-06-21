6785bb3d09f863e44316f0a4c52b044ee48643d7
whosNikoloz <nika.kobaidze021@gmail.com>
2025-06-21 15:00:19 +0400

    slug for categories

---
+++ app/[lang]/category/[...slug]/page.tsx
@@ -0,0 +1,23 @@
+import { title } from "@/components/primitives";
+
+interface CategoryPageProps {
+  params: {
+    slug?: string[];
+  };
+}
+
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const { slug = [] } = await params; 
+
+  const category = slug[0] || "Unknown Category";
+  const subcategory = slug[1] || null;
+
+  return (
+    <div>
+      <h1 className={title()}>
+        {subcategory ? `${category} / ${subcategory}` : category}
+      </h1>
+      <pre>{JSON.stringify(slug, null, 2)}</pre>
+    </div>
+  );
+}
